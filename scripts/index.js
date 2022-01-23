3a62afa21f5d723d8dfc7800f6d3ea91c7bdbb28
Arunit Mukherjee <69101964+arunit737@users.noreply.github.com>
2022-01-23 13:34:58 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 26, 2021',
+  birthdate: 'Jan 26, 2022',
   name: 'Ankita & Arunit'
 };
 
